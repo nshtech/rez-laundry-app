9158dba36260e530e3b0ac960647bd2463b27b80
Patrice Power <patricepower2021@u.northwestern.edu>
2020-05-13 11:14:38 -0400

    Added static data to customer sheet

--- src/components/CustomerSheet.js
+++ src/components/CustomerSheet.js
@@ -13,107 +13,18 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import timeGridPlugin from '@fullcalendar/timegrid';
 
+import customerData from '../customers.json'
+
 export class CustomerSheet extends Component {
 
     constructor() {
         super();
+
         this.state = {
             tasks: [],
             city: null,
             selectedCar: null,
-            lineData: {
-                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-                datasets: [
-                    {
-                        label: 'First Dataset',
-                        data: [65, 59, 80, 81, 56, 55, 40],
-                        fill: false,
-                        borderColor: '#007be5'
-                    },
-                    {
-                        label: 'Second Dataset',
-                        data: [28, 48, 40, 19, 86, 27, 90],
-                        fill: false,
-                        borderColor: '#20d077'
-                    }
-                ]
-            },
-            fullcalendarOptions: {
-                plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
-                defaultDate: '2017-02-01',
-                header: {
-                    left: 'prev,next today',
-                    center: 'title',
-                    right: 'month,agendaWeek,agendaDay'
-                },
-                editable: true
-            },
-            events: [
-                {
-                    "id": 1,
-                    "title": "All Day Event",
-                    "start": "2017-02-01"
-                },
-                {
-                    "id": 2,
-                    "title": "Long Event",
-                    "start": "2017-02-07",
-                    "end": "2017-02-10"
-                },
-                {
-                    "id": 3,
-                    "title": "Repeating Event",
-                    "start": "2017-02-09T16:00:00"
-                },
-                {
-                    "id": 4,
-                    "title": "Repeating Event",
-                    "start": "2017-02-16T16:00:00"
-                },
-                {
-                    "id": 5,
-                    "title": "Conference",
-                    "start": "2017-02-11",
-                    "end": "2017-02-13"
-                },
-                {
-                    "id": 6,
-                    "title": "Meeting",
-                    "start": "2017-02-12T10:30:00",
-                    "end": "2017-02-12T12:30:00"
-                },
-                {
-                    "id": 7,
-                    "title": "Lunch",
-                    "start": "2017-02-12T12:00:00"
-                },
-                {
-                    "id": 8,
-                    "title": "Meeting",
-                    "start": "2017-02-12T14:30:00"
-                },
-                {
-                    "id": 9,
-                    "title": "Happy Hour",
-                    "start": "2017-02-12T17:30:00"
-                },
-                {
-                    "id": 10,
-                    "title": "Dinner",
-                    "start": "2017-02-12T20:00:00"
-                },
-                {
-                    "id": 11,
-                    "title": "Birthday Party",
-                    "start": "2017-02-13T07:00:00"
-                },
-                {
-                    "id": 12,
-                    "title": "Click for Google",
-                    "url": "http://google.com/",
-                    "start": "2017-02-28"
-                }
-            ]
+            customers: customerData
         };
 
         this.onTaskChange = this.onTaskChange.bind(this);
@@ -142,19 +53,19 @@ export class CustomerSheet extends Component {
     render() {
         return (
             <div>
-                <div className="p-col-12 p-lg-6">
+                {/* <div className="p-col-12 p-lg-6"> */}
                     <div className="card">
                         <h1 style={{ fontSize: '16px' }}>Recent Sales</h1>
-                        <DataTable value={this.state.cars} style={{ marginBottom: '20px' }} responsive={true}
-                            selectionMode="single" selection={this.state.selectedCar} onSelectionChange={(e) => this.setState({ selectedCar: e.value })}>
-                            <Column field="vin" header="Vin" sortable={true} />
-                            <Column field="year" header="Year" sortable={true} />
-                            <Column field="brand" header="Brand" sortable={true} />
-                            <Column field="color" header="Color" sortable={true} />
+                        <DataTable value={this.state.customers} style={{ marginBottom: '20px' }} responsive={true} autoLayout={true}>
+                            <Column field="id" header="ID" sortable={true} />
+                            <Column field="name" header="Name" sortable={true} />
+                            <Column field="email" header="Email" sortable={true} />
+                            <Column field="phone" header="Phone" sortable={true} />
                         </DataTable>
                     </div>
-                </div>
+                {/* </div> */}
             </div>
         );
     }
 }
+// selectionMode = "single" selection = { this.state.selectedCar } onSelectionChange = {(e) => this.setState({ selectedCar: e.value })}
