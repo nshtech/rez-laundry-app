d61f5acc1f27188319008ac314e0a4866569307a
Caroline Lobel <57736173+carolinelobel@users.noreply.github.com>
2020-07-01 09:15:31 -0400

    Update BagTracker.js

--- src/components/BagTracker.js
+++ src/components/BagTracker.js
@@ -204,7 +204,14 @@ export class BagTracker extends Component {
 
     /* --------------- Filters ---------------- */
     statusBodyTemplate(rowData) {
-        return <span className={rowData.laundrystatus}>{rowData.laundrystatus.replace(/-/g,' ')}</span>
+        var laundryStatusDisplay = {
+            'picked-up': 'picked up',
+            'delivered-to-SH': 'delivered to SH',
+            'delivered-to-dorm': 'delivered to dorm',
+            'out-of-service': 'out of service',
+            'bag-missing': 'bag missing'
+        }
+        return <span className={rowData.laundrystatus}>{laundryStatusDisplay[rowData.laundrystatus]}</span>
     }
     weightBodyTemplate(rowData) {
         return <span className={rowData.weightstatus}>{rowData.weightstatus}</span>;
